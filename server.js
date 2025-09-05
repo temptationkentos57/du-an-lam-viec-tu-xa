207e1c45155332a81eb05f9fa1def54b17870e2f
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-05 20:23:09 +0200

    Update express error handling to respond with a JSON object.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(MONGODB_URI, mongooseOptions)
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).json({ error: 'Something broke!' });
 });
 
 app.listen(PORT, () => {
