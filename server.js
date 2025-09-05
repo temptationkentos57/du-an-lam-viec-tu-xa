daac73d465fceaf628a52df6b73e4f0f4af0ad31
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-05 20:22:20 +0200

    Updated MongoDB connection URI to use the new format and added error handling middleware.

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ mongoose.connect(MONGODB_URI, mongooseOptions)
   .then(() => console.log('Successfully connected to MongoDB'))
   .catch(err => console.error('MongoDB connection failed:', err.message));
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
